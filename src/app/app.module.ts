2c0b27881663489b36c0b881d559b81d2c1f0038
Anksoni02 <anksoni0205@gmail.com>
2022-08-01 16:07:17 +0530

    First
    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductsFeatureModule } from './products-feature/products-feature.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ProductsFeatureModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from '../auth-service.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  
+  formGroup!: FormGroup; 
+  isTrue:boolean = false ; 
+  constructor(private authservice : AuthServiceService) { }
+
+  ngOnInit(): void {
+    this.initForm() ; 
+  }
+
+  initForm(){
+            this.formGroup = new FormGroup({
+                email:new FormControl('',[Validators.required]),
+                password:new FormControl('',[Validators.required])
+            }) ; 
+  }
+  changeTheme(){
+    this.isTrue = !this.isTrue
+  }
+  loginProcess()
+  {
+      if(this.formGroup.valid)
+      {
+        this.authservice.login(this.formGroup.value).subscribe(result =>{
+              if(result.success)
+              {
+                  console.log(result); 
+                  alert(result.message);
+              }
+              else 
+              {
+                  alert(result.message) ; 
+              }
+        }); 
+      }   
+  }
+}
